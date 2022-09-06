1ccd0476b1a317fe6925f20e392a04c23f493b40
Om Patil <kushpo.030507@gmail.com>
2022-09-07 00:17:54 +0530

    exercise 1

--- chap1/ex_1.c
+++ chap1/ex_1.c
@@ -7,9 +7,9 @@ int main()
 	da=0.4*bs;
 	hr=0.2*bs;
 	gs=da+hr+bs;
-	printf("Basic salary of Ramesh: %f\n",bs);
-	printf("Dearness allowance of Ramesh: %f\n",da);
-	printf("House rent of Ramesh: %f\n",hr);
-	printf("Gross salary of Ramesh: %f\n",gs);
+	printf("Basic salary of Ramesh: %f\n", bs);
+	printf("Dearness allowance of Ramesh: %f\n", da);
+	printf("House rent of Ramesh: %f\n", hr);
+	printf("Gross salary of Ramesh: %f\n", gs);
 	return 0;
 }

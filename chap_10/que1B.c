02cf93b4e37c7c2652ed8213cab7ddc619f50a5a
Om Patil <kushpo.030507@gmail.com>
2022-10-04 20:00:04 +0530

    2nd part of Question 1

---
+++ chap_10/que1B.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <math.h>
+int bin (int , int );
+int main()
+{
+    int num, c = -1;
+    printf("enter number : ");
+    scanf("%d", &num);
+    printf("equialent binary = %d\n", bin(num, c));
+    return 0;
+}
+int bin (int num, int c)
+{
+    int a, sum;
+    if (!(num))
+        return 0;
+    else
+        {   
+            a = num % 2;
+            num = num / 2;
+            c++;
+            sum = a * pow(10,c) + bin (num, c);
+        }
+    return sum;
+    
+}

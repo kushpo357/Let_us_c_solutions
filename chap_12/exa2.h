505c4050bacfeab11defcc003dba9ab30bbd5cf2
Om Patil <kushpo.030507@gmail.com>
2022-10-06 21:09:00 +0530

    Header file for Program 2

---
+++ chap_12/exa2.h
@@ -0,0 +1,6 @@
+#define CAREA(r) (3.14 * r * r)
+#define CPERI(r) (2 * 3.14 * r * r)
+#define SAREA(a) (a * a)
+#define SPERI(a) (4 * a)
+#define TPERI(a,b,c) (a + b + c)
+#define TAREA(b,h) (b * h / 2)
